de27f9959d53969b4f870560f3457d98eb28bc6c
BetaCat_HA <wangjianyu2311077@gmail.com>
2024-01-18 21:47:14 +0800

    修复：主页图标加载不正确的Bug

--- Frontend/reader/my_page.py
+++ Frontend/reader/my_page.py
@@ -1,7 +1,7 @@
 # coding:utf-8
-from PyQt5.QtGui import QColor
+from PyQt5.QtGui import QColor, QPixmap
 from PyQt5.QtWidgets import QWidget, QGraphicsDropShadowEffect
-from qfluentwidgets import FluentIcon, setFont, InfoBarIcon
+from qfluentwidgets import FluentIcon, setFont, InfoBarIcon, Icon
 
 from Frontend.reader.Ui_MyPage import Ui_MyPage
 
@@ -11,3 +11,21 @@ class MyPage(Ui_MyPage, QWidget):
     def __init__(self, parent=None):
         super().__init__(parent=parent)
         self.setupUi(self)
+
+        # set the icon of button
+        self.pinPersonButton.setIcon(FluentIcon.PIN)
+        self.moreButton.setIcon(FluentIcon.MORE)
+        self.pinBorrowButton.setIcon(FluentIcon.PIN)
+
+        setFont(self.progressRing, 16)
+
+        # add shadow effect to card
+        self.setShadowEffect(self.BorrowInfoCard)
+        self.setShadowEffect(self.personalInfoCard)
+
+    def setShadowEffect(self, card: QWidget):
+        shadowEffect = QGraphicsDropShadowEffect(self)
+        shadowEffect.setColor(QColor(0, 0, 0, 15))
+        shadowEffect.setBlurRadius(10)
+        shadowEffect.setOffset(0, 0)
+        card.setGraphicsEffect(shadowEffect)
